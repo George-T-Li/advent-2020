1b06f61d828231dbd472291842ed26526eeb020f
tianzhi-george-li <tianzhi-george-li@users.noreply.github.com>
2022-02-19 19:28:14 -0500

    fix test names

--- Day2/test_password_philosophy.py
+++ Day2/test_password_philosophy.py
@@ -1,11 +1,11 @@
 import password_philosophy1, password_philosophy2, pytest
 
-def test_report_repair1():
+def test_password_philosophy1():
     file = "Day2/test_input.txt"
     result = password_philosophy1.main(file) 
     assert result == 2
 
-def test_report_repair2():
+def test_password_philosophy2():
     file = "Day2/test_input.txt"
     result = password_philosophy2.main(file)
     assert result == 1
--- Day3/test_toboggan_trajectory.py
+++ Day3/test_toboggan_trajectory.py
@@ -1,11 +1,11 @@
 import toboggan_trajectory1, toboggan_trajectory2, pytest
 
-def test_report_repair1():
+def test_toboggan_trajectory1():
     file = "Day3/test_input.txt"
     result = toboggan_trajectory1.main(file) 
     assert result == 7
 
-def test_report_repair2():
+def test_toboggan_trajectory2():
     file = "Day3/test_input.txt"
     result = toboggan_trajectory2.main(file)
     assert result == 336
